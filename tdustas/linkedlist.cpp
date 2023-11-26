c2a6540083e1f8823d6b452dd30fee294bb6fb3e
krishna.singh.2192003@gmail.com <krishna.singh.2192003@gmail.com>
2023-11-26 15:55:43 +0530

    remake

---
+++ tdustas/linkedlist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+    public:
+    Node();
+    Node(int a)
+    {data = a;}
+    
+};
+
+class LL{
+    Node* head;
+    public:
+    void insert(int data);
+    void display();
+    void display();
+};
+
+
+int main()
+{
+    LL obj;
+    return 0;
+}
