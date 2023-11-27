5a7455a40fea8735563751ea9fe668a299aaf77e
krishna.singh.2192003@gmail.com <krishna.singh.2192003@gmail.com>
2023-11-27 11:45:29 +0530

    Added new file GCD

--- jstuts/sample.js
+++ jstuts/sample.js
@@ -1,7 +1,4 @@
 for(let i=0;i<10;++i)
 console.log("suka");
-<<<<<<< HEAD
 for(let i=0;i<5;++i) console.log("ne nado izvinitsya")
-=======
-for(let i=0;i<5;++i) console.log("izvini")
->>>>>>> main
+for(let i=0;i<5;++i) console.log("
